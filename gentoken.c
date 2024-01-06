4bda490e62d20d714e3d3a1bc404f28492438156
Calebkillom <Calebkillom@gmail.com>
2024-01-06 18:14:54 +0300

    gentoken draft 2

--- gentoken.c
+++ gentoken.c
@@ -5,55 +5,45 @@
 
 char **genToken(int argc, char *argv[])
 {
-	int i, token_total;
+	int i = 0;
+	int token_total = 0;
 
-	i = 0;
-	token_total = 0;
-
-	/* enforce user to enter something in the command*/
 	if (argc < 2)
 	{
 		perror("Please Enter your Input!");
 		exit(EXIT_FAILURE);
 	}
 
-	/* Count the Number of Tokens in the input String */
 	char *token_count = strtok(argv[1], " ");
-	
+
 	while (token_count != NULL)
 	{
 		token_total++;
 		token_count = strtok(NULL, " ");
 	}
 
-	/* Dynamically allocate memory for an array of strings */
-	char **cmd_commands = (char **)malloc((token_count + 1) * sizeof(char *));
+	char **cmd_commands = (char **)malloc((token_total + 1) * sizeof(char *));
 
-	/* Check if Memory Allocation was successful. */
 	if (cmd_commands == NULL)
 	{
 		perror("Memory Allocation failed!");
 		exit(EXIT_FAILURE);
 	}
 
-	/* Extract the Input string and split it into Tokens */
 	char *token = strtok(argv[1], " ");
 
-	/* copy the current token to the allocated memory */
 	while (token != NULL)
 	{
 		cmd_commands[i] = strdup(token);
-
 		if (cmd_commands[i] == NULL)
 		{
 			perror("Memory allocation for a token failed");
 			exit(EXIT_FAILURE);
 		}
-
 		token = strtok(NULL, " ");
 		i++;
 	}
-	cmd_commands = NULL;
 
+	cmd_commands[token_total] = NULL;
 	return (cmd_commands);
 }
