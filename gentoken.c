45a0b9f53e26c38e34870eb93b7d0705bd586ad1
Calebkillom <Calebkillom@gmail.com>
2024-01-10 22:40:35 +0300

    task_3 corrections

--- gentoken.c
+++ gentoken.c
@@ -3,53 +3,63 @@
 #include <string.h>
 #include <errno.h>
 
-int countTokens(char *input)
+/*int countTokens(char *input)
 {
 	int token_total = 0;
-	char *tokenCount = strtok(input, " ");
+	char *tokenCount = strtok(input, " \n");
 
 	while (tokenCount != NULL)
 	{
 		token_total++;
-		tokenCount = strtok(NULL, " ");
+		tokenCount = strtok(NULL, " \n");
 	}
 
 	return (token_total);
-}
+}*/
 
 char **genToken(char *input)
 {
 	int i = 0;
-	int tokenTotal = countTokens(input);
-	char **cmd_commands, *token;
+	/*int tokenTotal = countTokens(input);*/
+	char **cmd_commands, **tmp, *token;
 
 	printf("Inside genToken\n");
 	printf("Input Line: %s\n", input);
 
-	cmd_commands = (char **)malloc((tokenTotal + 1) * sizeof(char *));
-	token = strtok(input, " \n");
+	cmd_commands = malloc(sizeof(char *));
 
 	if (cmd_commands == NULL)
 	{
 		perror("Memory Allocation failed!");
 		exit(EXIT_FAILURE);
 	}
-
+	token = strtok(input, " \n");
 	printf("Tokens: ");
 
 	while (token != NULL)
 	{
-		cmd_commands[i] = strdup(token);
+		tmp = realloc(cmd_commands, (i + 2) * sizeof(char *));
+		if (tmp == NULL)
+		{
+			perror("Memory allocation for a token failed");
+			exit(EXIT_FAILURE);
+		}
+		cmd_commands = tmp;
+
+		cmd_commands[i] = malloc(strlen(token) + 1);
+
 		if (cmd_commands[i] == NULL)
 		{
 			perror("Memory allocation for a token failed");
 			exit(EXIT_FAILURE);
 		}
-		token = strtok(NULL, " ");
+		strcpy(cmd_commands[i], token);
+		printf("%s ", cmd_commands[i]);
 		i++;
+		token = strtok(NULL, " \n");
 	}
 	printf("\n");
 
-	cmd_commands[tokenTotal] = NULL;
+	cmd_commands[i] = NULL;
 	return (cmd_commands);
 }
