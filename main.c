0d38c5c660bd149d71b75ba934596f941d75a0d5
Calebkillom <Calebkillom@gmail.com>
2024-01-12 12:22:01 +0300

    main.c betty compliance

--- main.c
+++ main.c
@@ -4,6 +4,15 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include "main.h"
+/**
+ * main - Entry point for the program.
+ *
+ * Description: This function serves as the entry point for the program.
+ * It initializes variables, prompts the user for input,
+ * tokenizes the input, and executes the corresponding command.
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
@@ -13,26 +22,26 @@ int main(void)
 	ssize_t read_line = 0;
 
 	while (prompt_and_read(display_prompt, &line, &len, &read_line))
-    {
-        char **tokens;
+	{
+		char **tokens;
 		int i;
 
-        /* Tokenize the input line into command and arguments */
-        tokens = genToken(line);
+		/* Tokenize the input line into command and arguments */
+		tokens = genToken(line);
 
-        /* Execute the command with arguments */
-        process_command(tokens, read_line);
+		/* Execute the command with arguments */
+		process_command(tokens, read_line);
 
-        /* Free memory allocated for tokens */
-        free(line);
-        line = NULL;
+		/* Free memory allocated for tokens */
+		free(line);
+		line = NULL;
 
-        for (i = 0; tokens[i] != NULL; i++)
-        {
-            free(tokens[i]);
-        }
-        free(tokens);
-    }
+		for (i = 0; tokens[i] != NULL; i++)
+		{
+			free(tokens[i]);
+		}
+		free(tokens);
+	}
 
 	return (0);
 }
