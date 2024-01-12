bda526acd2a69482fb26293d05f515deb8a68323
Calebkillom <Calebkillom@gmail.com>
2024-01-12 12:06:13 +0300

    prompt_and_read betty compliant code

--- prompt_and_read.c
+++ prompt_and_read.c
@@ -6,6 +6,15 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include "main.h"
+/**
+ * prompt_and_read - Displays a prompt, reads a line from stdin, and stores it.
+ * @display_prompt: The prompt to be displayed.
+ * @line: Pointer to the variable where the line will be stored.
+ * @len: Pointer to the variable storing the size of the allocated buffer.
+ * @read_line: Pointer to the variable where no of char read will be stored.
+ *
+ * Return: 1 if successful, 0 if EOF is encountered, exits on failure
+ */
 
 int prompt_and_read(const char *display_prompt, char **line, size_t *len,
 		ssize_t *read_line)
@@ -42,6 +51,13 @@ int prompt_and_read(const char *display_prompt, char **line, size_t *len,
 	return (1);
 }
 
+/**
+ * token_count - Counts the number of elements in an array of strings.
+ * @tokens: The array of strings.
+ *
+ * Return: The number of elements in the array.
+ */
+
 size_t token_count(char **tokens)
 {
 	size_t count = 0;
@@ -53,6 +69,13 @@ size_t token_count(char **tokens)
 
 	return (count);
 }
+
+/**
+ * execute_command - Executes a command using fork and execve.
+ * @full_path: The full path of the command to be executed.
+ * @tokens: The array of tokens representing the command.
+ * @read_line: The number of characters read from the input.
+ */
 
 void execute_command(char *full_path, char **tokens, ssize_t read_line)
 {
@@ -97,6 +120,12 @@ void execute_command(char *full_path, char **tokens, ssize_t read_line)
 		free(full_path);
 	}
 }
+
+/**
+ * process_command - Processes by searching for full path and executing it.
+ * @tokens: The array of tokens representing the command.
+ * @read_line: The number of characters read from the input.
+ */
 
 void process_command(char **tokens, ssize_t read_line)
 {
